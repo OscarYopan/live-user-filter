e7596dde5763f4335d334dd0f22ad8bafdf0141e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-10-25 12:46:16 -0300

    --

--- script.js
+++ script.js
@@ -10,9 +10,21 @@ async function getData() {
   const { results } = await res.json()
 
   //clear results
-  results.innerHTML = ''
+  result.innerHTML = ''
 
   results.forEach(user => {
-    console.log(user);
+    const li = document.createElement('li')
+
+    listItems.push(li)
+
+    li.innerHTML = `
+      <img src="${user.picture.large}" alt="${user.name.first}">
+      <div class="user-info">
+        <h4>${user.name.first} ${user.name.last}</h4>
+        <p>${user.location.city}, ${user.location.country}</p>
+      </div>
+    `
+
+    result.appendChild(li)
   });
 }
